092cca7d1f9c122456cadb207f1e10581fedc241
Salvador <salva.7693@gmail.com>
2018-10-29 19:31:54 -0300

    Ordenado

---
+++ TP_1_Calculadora/funciones.h
@@ -0,0 +1,11 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+float pedirNumero(char* mensaje);
+int sumar(int numUno, int numDos);
+int restar(int numUno, int numDos);
+float dividir(int numUno, int numDos);
+long multiplicar(int numUno, int numDos);
+double factorial(int numUno);
+
+#endif // FUNCIONES_H_INCLUDED
